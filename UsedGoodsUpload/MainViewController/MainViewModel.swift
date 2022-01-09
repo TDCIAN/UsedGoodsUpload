54b28998f7f23765e241c4c1e1c7dadc80fb14b9
TDCIAN <tdcian71@gmail.com>
2022-01-09 23:05:48 +0900

    add TitleTextFieldCell

---
+++ UsedGoodsUpload/MainViewController/MainViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewModel.swift
+//  UsedGoodsUpload
+//
+//  Created by JeongminKim on 2022/01/09.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct MainViewModel {
+    
+}
---
+++ UsedGoodsUpload/MainViewController/TitleTextFieldCell/TitleTextFieldCellViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TitleTextFieldCellViewModel.swift
+//  UsedGoodsUpload
+//
+//  Created by JeongminKim on 2022/01/09.
+//
+
+import RxCocoa
+
+struct TitleTextFieldCellViewModel {
+    let titleText = PublishRelay<String?>()
+}
